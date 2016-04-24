ebf42a91567a24d63f76c8812fc476d7c93f4e2a
przepompownia <przepompownia@users.noreply.github.com>
2016-04-24 16:39:03 +0200

    ClassFilter to create and validate search criteria
    It allows to set its content at the constructtion time only.

---
+++ php/ClassFilter.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace PHPCD;
+
+class ClassFilter
+{
+    private $criteria = [];
+
+    private $fields = [
+        'isAbstractClass',
+        'isFinal',
+        'isTrait',
+        'isInstantiable',
+        'isInterface'
+    ];
+
+    public function __construct(array $criteria)
+    {
+        foreach ($this->fields as $field) {
+            if (isset($criteria[$field])) {
+                $this->validateField($criteria[$field]);
+                $this->criteria[$field] = $criteria[$field];
+            } else {
+                 $criteria[$field] = null;
+            }
+        }
+
+        // @todo Disable it to normal usage
+        $this->validate();
+    }
+
+    public function getFieldNames()
+    {
+        return $this->fields;
+    }
+
+    private function validateField($field)
+    {
+        if ($field !== true && $field !== false && $field !== null) {
+            $message = sprintf('%s must be set to true, false or null.', (string)$field);
+            throw new \InvalidArgumentException($message);
+        }
+
+        return true;
+    }
+
+    /**
+     * Probably usable only as remainder
+     * develop calling requests the client
+     */
+    private function validate()
+    {
+        $wrongCombinations = [
+            ['isAbstractClass' => true, 'isTrait' => true],
+            ['isAbstractClass' => true, 'isInterface' => true],
+            ['isAbstractClass' => true, 'isFinal' => true],
+            ['isAbstractClass' => true, 'isInstantiable' => true],
+            ['isInstantiable' => true, 'isTrait' => true],
+            ['isInstantiable' => true, 'isInterface' => true],
+            ['isTrait' => true, 'isInterface' => true],
+            ['isTrait' => true, 'isFinal' => true],
+            ['isInterface' => true, 'isFinal' => true],
+            ['isInstantiable' => false, 'isFinal' => true]
+        ];
+
+        foreach ($wrongCombinations as $combination) {
+            $fields = [];
+            foreach ($combination as $field => $value) {
+                if ($this->criteria[$field] === $value) {
+                    $fields[] = $field;
+                }
+            }
+
+            if (count($fields) === count($combination)) {
+                $message = sprintf(
+                    'Bad search criteria: [%s] used at once.',
+                    implode(',', $fields)
+                );
+                throw new \InvalidArgumentException($message);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isAbstractClass()
+    {
+        return $this->criteria['isAbstractClass'];
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isFinal()
+    {
+        return $this->criteria['isFinal'];
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isTrait()
+    {
+        return $this->criteria['isTrait'];
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isInstantiable()
+    {
+        return $this->criteria['isInstantiable'];
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isInterface()
+    {
+        return $this->criteria['isInterface'];
+    }
+}
