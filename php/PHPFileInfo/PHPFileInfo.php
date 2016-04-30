012dd6f6138efce41b552462fc376c1e70a17db0
przepompownia <przepompownia@users.noreply.github.com>
2016-05-01 01:11:32 +0200

    interface PHPFileInfo - some new methods

--- php/PHPFileInfo/PHPFileInfo.php
+++ php/PHPFileInfo/PHPFileInfo.php
@@ -4,4 +4,31 @@ namespace PHPCD\PHPFileInfo;
 
 interface PHPFileInfo
 {
+    /**
+     * @return string
+     */
+    public function getNamespace();
+
+    /**
+     * @return string
+     */
+    public function getClass();
+
+    /**
+     * @return string
+     */
+    public function getImports();
+
+    /**
+     * @param array     $new_class_params {
+     *  @type string    $alias
+     *  @type string    $full_path
+     *  }
+     *
+     * @return array {
+     *  @type string        $alias        the original or modified alias
+     *  @type string|null   $full_path    null if we have no new import to do
+     *  }
+     */
+    public function getFixForNewClassUsage(array $new_class_params);
 }
