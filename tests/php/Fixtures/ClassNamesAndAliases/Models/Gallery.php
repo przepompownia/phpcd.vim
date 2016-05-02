079ea72fd081cf1571917b6d8448ba4936d49c55
przepompownia <przepompownia@users.noreply.github.com>
2016-05-02 03:38:36 +0200

    New fixture due to test created in 9155c1f

---
+++ tests/php/Fixtures/ClassNamesAndAliases/Models/Gallery.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PHPCD\Fixtures\ClassNamesAndAliases\Models;
+
+use PHPCD\Fixtures\ClassNamesAndAliases\Models\Image as Picture;
+use PHPCD\Fixtures\ClassNamesAndAliases\Models\Image as Photo;
+
+class Gallery
+{
+}
