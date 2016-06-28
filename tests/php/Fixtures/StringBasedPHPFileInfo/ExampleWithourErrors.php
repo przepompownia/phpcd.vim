939f2d490c12b09b3d0306efd105b69bafeeab64
przepompownia <przepompownia@users.noreply.github.com>
2016-06-29 00:54:11 +0200

    Added forgotten fixture

---
+++ tests/php/Fixtures/StringBasedPHPFileInfo/ExampleWithourErrors.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PHPCD\Fixtures\StringBasedPHPFileInfo;
+
+use PHPCD\Fixtures\ClassNamesAndAliases\Repositories\Category as Cat;
+
+class ExampleWithourErrors extends Cat implements I1, I2
+{
+}
