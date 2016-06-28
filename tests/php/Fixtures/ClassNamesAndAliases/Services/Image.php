1306c260bba91f08bf567dcbbcebc7cb0f699901
przepompownia <przepompownia@users.noreply.github.com>
2016-06-28 22:05:05 +0200

    Tests: line specific for PHP7 was commented

--- tests/php/Fixtures/ClassNamesAndAliases/Services/Image.php
+++ tests/php/Fixtures/ClassNamesAndAliases/Services/Image.php
@@ -3,7 +3,7 @@
 namespace PHPCD\Fixtures\ClassNamesAndAliases\Services;
 
 use PHPCD\Fixtures\ClassNamesAndAliases\Repositories\Category as Cat;
-use A\B\{C as X, D, E as F};
+// use A\B\{C as X, D, E as F};
 
 class Image
 {
