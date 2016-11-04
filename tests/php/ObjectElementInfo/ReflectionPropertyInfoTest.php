cfa69f5fefdba6703a69ed545525017a47247ef3
przepompownia <przepompownia@users.noreply.github.com>
2016-11-04 09:46:00 +0100

    fixup! [squash it]

--- tests/php/ObjectElementInfo/ReflectionPropertyInfoTest.php
+++ tests/php/ObjectElementInfo/ReflectionPropertyInfoTest.php
@@ -10,23 +10,33 @@ class ReflectionPropertyInfoTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider getAllowedTypesDataProvider
      */
-    public function getAllowedTypes()
+    public function getAllowedTypes($class, $method)
     {
         $docBlockFactory = DocBlockFactory::createInstance();
-        $property = new \ReflectionProperty(Test1::class, 'pub1');
+        $property = new \ReflectionProperty($class, $method);
         $propertyInfo = new ReflectionPropertyInfo($property, $docBlockFactory);
         $types = $propertyInfo->getAllowedTypes()->getType();
 
-        var_dump((string)$types);
+        var_dump(explode('|', (string)$types));
+
 
         $index = 0;
         while ($types->has($index)) {
             $type = $types->get($index);
-            var_dump((string)$type);
+            var_dump(sprintf("%s: %s\n", get_class($type), (string)$type));
             ++$index;
         }
 
         // var_dump($types);
+    }
+
+    public function getAllowedTypesDataProvider()
+    {
+        return [
+            [Test1::class, 'pub1'],
+            [Test1::class, 'pub2'],
+        ];
     }
 }
